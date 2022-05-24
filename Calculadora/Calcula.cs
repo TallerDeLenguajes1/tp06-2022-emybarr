42f13af9a97851068030f9c728d3edd77f8e67d3
emybarr <eybarreiro.2a@gmail.com>
2022-05-24 16:30:20 -0300

    punto1

---
+++ Calculadora/Calcula.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calcula
+    {
+        private double numero1;
+        private double numero2;
+        
+
+        public double Numero1 { get => numero1; set => numero1 = value; }
+        public double Numero2 { get => numero2; set => numero2 = value; }
+      
+
+        public double sumar()
+    {
+    return Numero1 + Numero2 ; 
+    }
+    
+    public double restar()
+    {
+        return  Numero1 - Numero2 ; 
+    }
+
+    public double multiplicar()
+    {
+        return Numero1 * Numero2;
+    }
+
+    public double dividir()
+    {
+        return Numero1 / Numero2;
+        
+    }
+
+     public int  Limpiar(){
+        return 0;
+
+    }
+}
+}
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Calculadora;
+
+
+Calcula calcu = new Calcula();
+int operador, continuar = 1 ; 
+double resultado;
+
+do{
+    Console.WriteLine("Elegir la Opecion a Realizar ");
+    do{
+        Console.WriteLine(" 1-SUMA\n 2-RESTA \n 3-MULTIPLICACION\n 4-DIVISION \n ");
+        operador=Convert.ToInt32(Console.ReadLine());
+    }while(operador < 1 || operador > 4);
+
+
+
+ Console.WriteLine("Ingreser un numero : ");
+calcu.Numero1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingreser un numero : ");
+calcu.Numero2=Convert.ToDouble(Console.ReadLine());
+
+switch(operador){
+    case 1 : 
+    resultado=calcu.sumar();
+    Console.WriteLine($"El resultado es {resultado}");
+    break;
+    case 2 : 
+    resultado=calcu.restar();
+   Console.WriteLine($"El resultado es {resultado}");
+    break;
+    case 3 : 
+    resultado=calcu.multiplicar();
+    Console.WriteLine($"El resultado es {resultado}");
+    break;
+    case 4 : 
+    if(calcu.Numero2 == 0 ){
+         Console.WriteLine("error");
+    }else{
+    resultado=calcu.dividir();
+    Console.WriteLine($"El resultado es {resultado}");
+    }
+    break;
+    
+
+}
+ calcu.Numero1= calcu.Limpiar();
+ calcu.Numero2 = calcu.Limpiar();
+ resultado =  calcu.Limpiar();
+
+
+  do{ 
+        Console.WriteLine("Seguir usando la calculadoa 1-SI ");
+        continuar=Convert.ToInt32(Console.ReadLine());
+
+    }while(continuar == 0);
+
+    Console.WriteLine($"numero 1 {calcu.Numero1} || numero 2 {calcu.Numero2}" ) ;
+
+}while(continuar ==1);
+
+
+
